8ec0d50195066be79d960eb68c52f7873f4ef763
MarcoB <mrc.bsllt@gmail.com>
2021-02-03 17:56:39 +0100

    completato il footer statico, devo ricordare di usare order per il responsive

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -18,9 +18,42 @@
       </p>
     </div>
 
-    <div class="central"></div>
-
-    <div class="right"></div>
+    <div class="right">
+      <div class="list">
+        <h3>Pastifico</h3>
+        <ul>
+          <li><a href="#">Il Pastificio</a></li>
+          <li><a href="#">Grano decorticato a pietra</a></li>
+          <li><a href="#">Il Molise c'è</a></li>
+          <li><a href="#">Filiera Integrata</a></li>
+          <li><a href="#">100 anni di pasta</a></li>
+          <li><a href="#">Sartoria della pasta</a></li>
+          <li><a href="#">Spaghetto Quadrato</a></li>
+          <li><a href="#">Le Persone</a></li>
+        </ul>
+      </div>
+      <div class="list">
+        <h3>Collezione da chef</h3>
+        <ul>
+          <li><a href="#">Collezione da Chef</a></li>
+          <li><a href="#">Grandi Cucine</a></li>
+          <li><a href="#">Biologiche</a></li>
+          <li><a href="#">Quadrate</a></li>
+        </ul>
+      </div>
+      <div class="list">
+        <h3>Prodotti</h3>
+        <ul>
+          <li><a href="#">Le classiche</a></li>
+          <li><a href="#">Le Integrali</a></li>
+          <li><a href="#">Le Speciali</a></li>
+          <li><a href="#">Le Biologihe</a></li>
+          <li><a href="#">Le Gluten-Free</a></li>
+          <li><a href="#">Le Semole</a></li>
+          <li><a href="#">Le Extra Lusso</a></li>
+        </ul>
+      </div>
+    </div>
 
   </div>
 </footer>
