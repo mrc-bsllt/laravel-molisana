0216eedfa5e2f7e77647d14730a32ce16f47d43d
MarcoB <mrc.bsllt@gmail.com>
2021-02-03 16:24:06 +0100

    completata la parte sinistra del footer (statico)

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,7 +1,23 @@
 <footer>
   <div class="container">
 
-    <div class="left"></div>
+    <div class="left">
+      <img src="{{ asset('img/marchio-sito-test.png') }}" alt="logo-molisana">
+      <p>
+        Ragione sociale: La Molisana S.P.A.<br>
+        Sede legale: Contrada Colle delle Alpi, 100/A<br>
+        86100 - Campobasso(CB)<br>
+        Pec: lamolisana@pec.it<br>
+        Tel: +39 0874 4981<br>
+        Fax: +39 0874 629584<br>
+        info@lamolisana.it (per segnalazioni egli utenti)<br>
+        commerciale@lamolisana.it<br>
+        export@lamolisana.it<br>
+        numero verde 800818081<br>
+        telefono 3801292455<br>
+      </p>
+    </div>
+
     <div class="right"></div>
 
   </div>
