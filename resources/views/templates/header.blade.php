c0dea5e11e854f7a5fc1e6cbe95d1896890e8927
MarcoB <mrc.bsllt@gmail.com>
2021-02-03 20:36:37 +0100

    completata la parte responsive

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,8 +1,13 @@
 <header>
   <img src="{{ asset('img/marchio-sito-test.png') }}" alt="logo-molisana">
-  <ul id="nav-list" class="list-inline">
+  <ul id="nav-list" class="list-inline visible-lg-block">
     <li><a href="#">Home</a></li>
     <li class="active"><a href="#">Prodotti</a></li>
     <li><a href="#">News</a></li>
   </ul>
+  <div class="hamburger hidden-lg">
+    <a href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+  </div>
 </header>
